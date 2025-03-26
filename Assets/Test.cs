3fb111e2ac42e184de87ab78b425f8d57511c0b6
clydestories <33929612+clydestories@users.noreply.github.com>
2025-03-26 16:47:48 +0300

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Привет из браузера");
     }
 
     // Update is called once per frame

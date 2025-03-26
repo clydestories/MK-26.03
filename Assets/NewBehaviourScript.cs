2198bb554fee6c3365278d80ed1c3621e0724ab0
clydestories <rogulkov2013@yandex.ru>
2025-03-26 16:42:52 +0300

    Made test changes

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Привет");
     }
 
     // Update is called once per frame
